a97eddf82a8ce00466c2f4b1c08be52640063f8c
David Israel <theovercomer8@gmail.com>
2023-02-02 19:09:11 -0500

    Fixing error

--- utils/caption.py
+++ utils/caption.py
@@ -111,7 +111,7 @@ class Config:
 class Interrogator():
     def __init__(self, config: Config):
         self.config = config
-        self.device = config.device
+        self.device = pytgitdevice
 
         if blip_pass:
             if config.blip_model is None:
